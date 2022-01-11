68fd5cd1520588ed885c57e0aed37c3e0be0386a
dravate <42339478+dravate@users.noreply.github.com>
2022-01-11 11:05:37 +0530

    Create taking_input_fromuser.py

---
+++ taking_input_fromuser.py
@@ -0,0 +1,20 @@
+#taking_input_fromuser.py
+
+val = input("Enter your value: ")
+print(val)
+
+"""
+check input
+# type in Python
+
+num = input ("Enter number :")
+print(num)
+name1 = input("Enter name : ")
+print(name1)
+
+# Printing type of input value
+print ("type of number", type(num))
+print ("type of name", type(name1))
+
+"""
+
