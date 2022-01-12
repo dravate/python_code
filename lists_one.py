e7a187dc367ad001fb6fe197ed025d4a63e9e40e
dravate <42339478+dravate@users.noreply.github.com>
2022-01-12 12:44:52 +0530

    Create lists_one.py

---
+++ lists_one.py
@@ -0,0 +1,20 @@
+#lists_one.py
+L = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+print (len(L))
+
+item = L[5]
+
+print (item)
+
+seq = L[2:7]
+
+print (seq)
+
+seq_step = L[1:8:2]
+print (seq_step)
+
+seq_trick = L[::2]
+print (seq_trick)
+
+© 2022 GitHub, Inc.
+Terms
