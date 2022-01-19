6feed290e88b9d526a22d25eb01a9350ca64744a
dravate <42339478+dravate@users.noreply.github.com>
2022-01-19 12:29:48 +0530

    Create function_demo_docstring.py

---
+++ function_demo_docstring.py
@@ -0,0 +1,14 @@
+#function_demo_docstring.py
+name = "Hari Sadu"
+
+def hellofunction(name=None):
+    '''Hello Function Demonstration'''
+    if name: 
+        print ("Hello ", name, "!")
+    else: 
+        print ("Hello Function!")
+
+
+print (hellofunction.__doc__)
+
+
