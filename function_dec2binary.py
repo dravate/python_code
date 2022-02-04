a7e5fa406590432ab007b0108fe11ee92aa1153d
dravate <42339478+dravate@users.noreply.github.com>
2022-02-04 15:03:19 +0530

    Update function_dec2binary.py

--- function_dec2binary.py
+++ function_dec2binary.py
@@ -3,6 +3,6 @@ def decToBin(n):
         if n == 0:
                 return '0'
         else:
-                decToBin(int(n/2)) + str(n % 2)
+               return  decToBin(int(n/2)) + str(n % 2)
 
 d = decToBin(12)
